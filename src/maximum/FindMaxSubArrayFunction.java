dd1065c0c360e1a30b92fd23950d0ef19c70567c
chaninkou <michaelkoulove@yahoo.com>
2019-12-28 21:49:34 -0800

    Great review on finding max using dynamic programming or variable O(n)

--- src/maximum/FindMaxSubArrayFunction.java
+++ src/maximum/FindMaxSubArrayFunction.java
@@ -1,12 +1,13 @@
 package maximum;
 
 public class FindMaxSubArrayFunction {
-	// O(n)
+	// O(n) Reason why this solution works because we are just trying to get the largest sum, but not the index
 	public int maxSubArray(int[] nums) {
 		// Start from the first element
 		int currentMax = nums[0];
 		int maxEndHere = nums[0];
 
+		// Starting from index 1 cause index 0 is already the max
 		for (int i = 1; i < nums.length; i++) {
 			// Bottom up approach
 			// Similar to maximum product sub array
@@ -19,19 +20,24 @@ public class FindMaxSubArrayFunction {
 		return currentMax;
 	}
 
-	// Another way to do it by creating dummy array
-	// public int maxSubArray(int[] nums){
-	// int[] dp = new int[nums.length];
-
-	// dp[0] = nums[0];
-
-	// int currentMax = dp[0];
-
-	// for(int i = 1; i < nums.length; i++){
-	// dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-	// currentMax = Math.max(currentMax, dp[i]);
-	// }
-
-	// return currentMax;
-	// }
+	// Another way to do it by using dynamic programming
+//	 public int maxSubArray(int[] nums){
+//		 int[] dp = new int[nums.length];
+//	
+//		 dp[0] = nums[0];
+//	
+//		 // Also starts from index 0
+//		 int currentMax = dp[0];
+//	
+//		 // Same as above
+//		 for(int i = 1; i < nums.length; i++){
+//			 // dp[i-1] is the same thing as the previous
+//			 dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+//			 
+//			 // since dp[i] will be the current sum end
+//			 currentMax = Math.max(currentMax, dp[i]);
+//		 }
+//	
+//		 return currentMax;
+//	 }
 }
